477b96381e661bf4071b3f7b53507c977e8422d1
TasmiaIshrat <chaad.ishrat@gmail.com>
2021-10-16 14:34:13 +0600

    version-2

--- main.js
+++ main.js
@@ -1,76 +1,90 @@
 var myArray = [];
 
-
-
-window.onload = function WindowLoad(event) {
+function showArrayStatus() {
 
     let text = "[ " + myArray.toString() + " ]";
     document.getElementById("display").innerHTML = text;
 
+}
+
+
+function getInputValue(){
+    return  document.getElementById("input_element").value;
+    
+}
+
+
+function clearInputField(){
+    document.getElementById("input_element").value = "" ;
+}
+
+
+window.onload = function WindowLoad(event) {
+
+    showArrayStatus();
 
 }
 
 
 function add_input_to_array() {
-    if(document.getElementById("input_element").value){
 
-    
-    console.log(document.getElementById("input_element").value);
-    myArray.push(document.getElementById("input_element").value);
-    let text = "[ " + myArray.toString() + " ]";
-    document.getElementById("display").innerHTML = text;
-    document.getElementById("input_element").value="";
-    }
-    else{
+    if (!getInputValue()) {
         alert("input Field is Empty!");
+        return;
     }
+
+    myArray.push(getInputValue());
+    showArrayStatus();
+    clearInputField();
+
+
 
 }
-function add_input_to_first_of_araay(){
-    if(document.getElementById("input_element").value){
-    
-    myArray.unshift(document.getElementById("input_element").value);
-    let text = "[ " + myArray.toString() + " ]";
-    document.getElementById("display").innerHTML = text;
-    document.getElementById("input_element").value="";
+
+
+function add_input_to_first_of_araay() {
+    if (getInputValue()) {
+
+        myArray.unshift(getInputValue());
+        showArrayStatus();
+        clearInputField();
     }
 }
 
 function add_last_to_array() {
-    if(document.getElementById("input_element").value){
-    myArray.push(document.getElementById("input_element").value);
-    let text = "[ " + myArray.toString() + " ]";
-    document.getElementById("display").innerHTML = text;
-    document.getElementById("input_element").value="";
+    if (getInputValue()) {
+        myArray.push(getInputValue());
+        showArrayStatus();
+        clearInputField();
     }
 
 }
 
 
-function delete_first_of_araay(){
-     myArray.shift();
-     let text = "[ " + myArray.toString() + " ]";
-    document.getElementById("display").innerHTML = text;
+function delete_first_of_araay() {
+    myArray.shift();
+    showArrayStatus();
 }
 
-function delete_last_of_araay(){
+function delete_last_of_araay() {
 
     myArray.pop();
-    let text = "[ " + myArray.toString() + " ]";
-    document.getElementById("display").innerHTML = text;
-    
+    showArrayStatus();
+
 
 }
-function add_index(){
-    if(document.getElementById("input_element").value && document.getElementById("input_index").value ){
 
-    myArray.splice(document.getElementById("input_index").value, 0, document.getElementById("input_element").value);
-    let text = "[ " + myArray.toString() + " ]";
-    document.getElementById("display").innerHTML = text;
+
+function add_index() {
+    if (getInputValue() && document.getElementById("input_index").value) {
+
+        myArray.splice(document.getElementById("input_index").value, 0, getInputValue());
+        showArrayStatus();
     }
 }
-function clear_araay(){
-    myArray=[];
-    let text = "[ " + myArray.toString() + " ]";
-    document.getElementById("display").innerHTML = text;
+
+
+function clear_araay() {
+    myArray = [];
+    showArrayStatus();
 }
